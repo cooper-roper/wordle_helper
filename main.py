df33d81536f4919ba180b7a6849360f5fbe6ba31
Cooper Roper <55814602+cooper-roper@users.noreply.github.com>
2022-12-14 17:23:59 -0800

    Update main.py
    updated and current

--- main.py
+++ main.py
@@ -1,137 +1,65 @@
-
-def main():	
-	while(1):
-		wordle()
-
-def wordle():
-	prompt = "\nEnter 5 letter word. Type ? to indicate unknown characters. If no greens, type '?????'. Also type ? for yellow spaces. We will fill those in later:\n\n"
-
-	file = open("wordlist.txt", "r")
-	list= file.read().split('\n')
-
-	res = []
-	check = True
-	while(check):
-		word = input(prompt)
-		word = word.strip()
-		length = len(word)
-		if length == 0:
-			prompt = ""
-		elif word == "exit":
-			exit()
-		elif length != 5:
-			prompt = "invalid. Try again:\n\n"
-		else:
-			check = False
-
-	if(word == "?????"):
-		result = list
-	else:
-		i = 0
-		for char in word:
-			if char != '?': 
-				l = []
-				if char.isalpha():
-					char = char.lower()
-					for elem in list:
-						if char == elem[i]:
-							l.append(elem)
-					res.append(l)
-				else:
-					print("found invalid character")
-					return	
-			i+=1
+def main():
+	with open('wordlist.txt') as f:
+	    word_list = f.read().splitlines()
 	
-		result = set.intersection(*map(set,res))
+	while(True):
+		state = {
+			"green": "\nEnter your green results. Type \"?\" in the positions of yellow or grey results. If no greens, type '?????':\n\n", 
+			"yellow": "\n\nEnter yellow results. Same format as earlier, '?' for greens or greys. If no yellows, hit enter:\n", 
+			"grey": "\n\nEnter the grey letters that you have used:\n\n"
+		}
 
-	if len(result) == 1:
-		print("the word is ", result)
-		return
-	elif len(result) > 1:
-		print("possible words:")
-		print("__________________________________________")
-		print(result)
-	else:
-		print("No words")
-		return
-
-	prompt = "\n\nEnter yellow letters. Same format as earlier, '?' for greens or grey. If none, hit enter. After, we will check available keyboard letters. Type 'exit' to exit\n\n"
-	yellows = input(prompt)
-	yellows = yellows.strip()
-	if yellows == "exit":
-		exit()
-	elif len(yellows) == 0 or yellows == "?????":
-		y_result = result
-					
-	elif(len(yellows) > 5):
-		print("invalid input or too many yellows")
-		return
-	
-
-	else:
-		i = 0
-		y_result = []
-		for char in yellows:
-			if char != '?': 
-				l = []
-				if char.isalpha():
-					char = char.lower()
-					for r in result:
-						if r.find(char) != i and r.find(char) != -1:
-							l.append(r)
-					y_result.append(l)
-				else:
-					print("found invalid character")
-					return	
-			i+=1
-	
-		y_result = set.intersection(*map(set,y_result))
-
-	if len(y_result) == 1:
-		print("the word is ", y_result)
-		return
-	elif len(y_result) > 1:
-		print("possible words:")
-		print("__________________________________________")
-		print(y_result)
-	else:
-		print("No words")
-		return
-
-		
-	prompt = "\n\nEnter the letters that you have used:\n\n"
-	greys = input(prompt)
-	greys = greys.strip()
-	if greys == "exit":
-		exit()
-	elif greys.isalpha():
-		g_result = []
-		for char in greys:
-			char = char.lower()
-			l = []
-			for r in y_result:
-				if(r.find(char) == -1):
-					l.append(r)
-			g_result.append(l)
+		for _ , (color, prompt) in enumerate(state.items()):
+			guess = valid_input(prompt, color)
 			
+			word_list = [word for word in word_list if checker(guess, word, color)]
+		
+			if (check_list(word_list) == -1):
+				with open('wordlist.txt') as f:
+	   				word_list = f.read().splitlines()
+				break
+
+def valid_input(prompt, color):
+	while(True):
+		word = ''.join([char.lower() for char in input(prompt).strip() if not char.isdigit()]) 
+		if (len(word) == 5 or color == "grey"):
+			return word
+		if word == "exit":
+			exit()
+		prompt = "invalid. Try again:\n\n"
+
+def check_list(res):
+
+	if len(res) > 0:
+		print("possible words: \n__________________________________________\n", res)
+		return 0
 	else:
-		print("invalid input or too many yellows")
-		return
+		print("No words, restarting script\n")
+		return -1
 
-	g_result = set.intersection(*map(set,g_result))
-	if len(g_result) == 1:
-		print("the word is ", g_result)
-	elif len(g_result) > 1:
-		print("possible words:")
-		print("__________________________________________")
-		print(g_result)
-	else:
-		print("No words")
+def checker(guess, word, color):
+	
+	for i, char in enumerate(guess):
+		
+		if(char == "?"):
+			continue
+		
+		elif(not char.isalpha()):
+			print("Unknown character: fatal error. Exiting...\n")
+			exit(-1)
+		
+		elif(color == "green" and char != word[i]):
+			return False
+		
+		elif(color == "yellow" and (char not in word or char == word[i])):
+			return False
 
-	print("__________________________________________\n\n\n")
-	return
+		elif(color == "grey" and char in word):
+			return False
 
-
+	return True
+			
+	
 
 if __name__ == "__main__":
     main()
